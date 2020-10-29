d61e18c948cb80ff196779ffbafb7d158dd7ce2b
jigspatel_it <jigspatel.04@gmail.com>
2020-10-29 18:20:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+import { useEffect } from 'react';
+
+function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('The User is >>>', authUser);
+      if (authUser) {
+        // the user just logged in
+        dispatch({
+          type: 'SET_USER',
+          user:authUser
+        });
+      } else {
+        // the user logged out
+        dispatch({
+          type: 'SET_USER',
+          user:null
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/checkout'>
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
